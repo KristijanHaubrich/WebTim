8f4fde2e27da2dd24bc74cf7eec0d145c8fd2ab7
Kvesss <david.kvesic99@gmail.com>
2022-04-08 10:15:07 +0200

    maven project, firebase config

---
+++ server/WebProjekt/src/main/java/com/example/firebase/SpringBootStarterApplication.java
@@ -0,0 +1,30 @@
+package com.example.firebase;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+@SpringBootApplication
+public class SpringBootStarterApplication {
+
+    public static void main(String[] args) throws IOException {
+        ClassLoader classLoader = SpringBootStarterApplication.class.getClassLoader();
+
+        File file = new File(Objects.requireNonNull(classLoader.getResource("serviceKey.json")).getFile());
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        SpringApplication.run(SpringBootStarterApplication.class, args);
+    }
+}
